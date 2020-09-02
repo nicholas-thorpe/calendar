9fd07437e95e68ca060d7a19fcb02e1b075435b7
Nick Thorpe <nicholas_thorpe@mhu.edu>
2020-09-02 15:01:46 -0400

    Added basic stuff

--- calendar.php
+++ calendar.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 
+<!-- Insert honor pledge here -->
+
 <html lang = "en-US">
 	<head>
 		<meta charset = "utf-8">
@@ -7,5 +9,25 @@
 	</head>
 	<body>
 		<h1>Calendar Generator</h1>
+		
+		<form action = "">
+			<label for = "Year">Year</label>
+			<input type = "text" name = "year" value = ""/>
+			
+			<label for = "Month">Month"</label>
+			<select name = "month">
+				<?php
+					for ($1 = 0; $i < 13; $i++) {
+						
+					}
+				?>
+			</select>
+			
+			<button type = "submit" value = "Submit">Submit</button>
+			<button type = "button"><a href = "<?php echo basename($_SERVER['PHP_SELF'];?>">Reset</a></button>
+		</form>
+		<div id = "results">
+			
+		</div>
 	</body>
 </html>
