c4192bbe0301af9f2126210229c126acf8cd9b45
Nick Thorpe <nicholas_thorpe@mhu.edu>
2020-09-07 14:55:21 -0400

    Fleshed out functions slightly

--- calendarFunctions.php
+++ calendarFunctions.php
@@ -1,6 +1,22 @@
+<?php
+/*
+	Checks if a given year-month combination is valid
+*/
+function isValid($year, $month) {
+
+}
+
 /*
 	Finds the day of the week of the first day of the month
 */
-function weekStart() {
+function weekStart($year, $month) {
+	
+}
+
+/*
+	
+*/
+function displayCalendar() {
 
 }
+?>
