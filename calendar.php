a5ed26123d3b9c4173017cbdf68e9741948a9f5c
Nick Thorpe <nicholas_thorpe@mhu.edu>
2020-09-09 20:04:25 -0400

    Finished calendar functions and everything else

--- calendar.php
+++ calendar.php
@@ -11,12 +11,14 @@
 		<?php require "calendarFunctions.php"?>
 		<?php
 			//Declare time variables using POST
+			//The function could also work with GET but POST is better
 			$year = $_POST['year'];
 			$month = $_POST['month'];
 		?>
 		
 		<style>
 			<?php
+				//Don't display results if the user hasn't done anything or reset
 				if (!$year && !$month)
 					echo "#results {display: none;}";
 			?>
@@ -26,9 +28,12 @@
 		<h1>Calendar Generator</h1>
 		
 		<form method = "post" action = "<?php echo basename($_SERVER['PHP_SELF']);?>">
+			<!-- Do I have to comment on everything cause if so this is gonna be annoying -->
+			<!-- Oh well, this is what's known as a "label" and a "text box" -->
 			<label for = "year">Year</label>
 			<input type = "text" name = "year" value = ""/>
 			
+			<!-- The label needs no explaining but there's something funky with that select -->
 			<label for = "month">Month</label>
 			<select name = "month">
 				<?php
@@ -47,25 +52,28 @@
 					echo "<option value = '13'>Whole Year</option>";
 				?>
 			</select>
+			<!-- Whew -->
 			
+			<!-- One tries to print a calendar, one tries to not print anything -->
 			<button type = "submit" value = "Submit">Submit</button>
 			<button type = "button"><a href = "<?php echo basename($_SERVER['PHP_SELF']); ?>">Reset</a></button>
 		</form>
+		
 		<div id = "results">
 			<?php
+				//Hannah added this and I don't know why but OK
+				date_default_timezone_set("America/New_York");
+				
+				//If it's an invalid year, the script freaks out
+				//Otherwise, it prints things like the levelheaded child it is
 				if (!isValid($year)) {
-			?>
-			
-			<h2 id = "error">Error</h2>
-			
-			<?php
+					echo "<h2 id = \"error\">Error</h2>";
+					echo "<p>ERROR: Year must be 1583 or later</p>";
+				} else if ($month == 13) {
+					yearCalendar($year);
 				} else {
-			?>
-			
-			<h2 id = "success"><?php echo $year;?></h2>
-			
-			<?php
-				generateCalendar($year, $month, );
+					monthCalendar($year, $month);
+				}
 			?>
 		</div>
 	</body>
--- calendarFunctions.php
+++ calendarFunctions.php
@@ -8,18 +8,15 @@ function isValid($year) {
 }
 
 /*
-	
+	Prints a single month calendar in a table
 */
-function displayCalendar($year, $month) {
-	//Creates needed variables
+function monthCalendar($year, $month) {
+	//Gets the date in a format PHP likes
 	$date = strtotime("$month/1/$year");
-	$weeks = date("w", $Date);
-	$days = date("t", $Date);
 	
-	//Adds labels
+	//Add year/month headings
 	$printMonth = date("F", $date);
-	echo "$printMonth\n";
-	echo "$year\n";
+	echo "<h2 id = \"success\">$printMonth $year</h2>";
 	
 	//Begin the table and load the day headings
 	echo "<table>";
@@ -33,24 +30,43 @@ function displayCalendar($year, $month) {
 	echo "<th>Saturday</th>";
 	echo "</tr>";
 	
+	//Variable to track the number of blank days at the start of a given month
+	$blankDays = date("w", $date);
+	
+	//Variable to track the number of days in a given month
+	$days = date("t", $date);
+
+	//Variable to track where in the week the script is
+	$weekTrack = 0;
+	
 	//Start the first row and load the leading empty spaces
 	echo "<tr>";
-	for($i = 0; $i < $weeks; $i++) {
+	for($i = 0; $i < $blankDays; $i++) {
 		echo "<td>&nbsp</td>";
-		$weeks = $i;
+		$weekTrack++;
 	}
 	
 	//Load in the actual days
-	for($i = 1; $i <= $daysInMonth; $i++) {
-		echo "<td>$i</td>";
-		$weeks++;
+	for($i = 0; $i < $days; $i++) {
+		$number = ($i + 1);
+		echo "<td>$number</td>";
 		
-		if(($weeks % 7) == 0)
+		$weekTrack++;
+		if(($weekTrack % 7) == 0)
 			echo "</tr> <tr>";
 	}
 	
 	//Close out the table	
 	echo "</tr>";
 	echo "</table>";
+}
+
+/*
+	Prints a full year calendar in several tables
+*/
+function yearCalendar($year) {
+	//Calls the month calendar function for every month in a given year
+	for ($i = 0; $i < 12; $i++)
+		monthCalendar($year, ($i + 1));
 }
 ?>
